72a160314ae039d886a21c3007a77ddaa21b0083
Denis Sholkovy <denissholkovy@gmail.com>
2024-02-06 16:49:53 +0200

    fix

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const tabs = {
-    btn: document.querySelectorAll('.tab'),
-    contentList: document.querySelectorAll('.content-item'),
-    check: (btn) => {
-        const index = Array.prototype.indexOf.call(tabs.btn ,btn);
-        tabs.btn.forEach((btn) => btn.classList.remove('active'));
-        tabs.contentList.forEach((btn) => btn.classList.remove('active'));
-        tabs.btn[index].classList.add('active');
-        tabs.contentList[index].classList.add('active');
-    }
-}
+  btn: document.querySelectorAll(".tab"),
+  contentList: document.querySelectorAll(".content-item"),
+  check: (btn) => {
+    const index = Array.prototype.indexOf.call(tabs.btn, btn);
+    tabs.btn.forEach((btn) => btn.classList.remove("active"));
+    tabs.contentList.forEach((btn) => btn.classList.remove("active"));
+    tabs.btn[index].classList.add("active");
+    tabs.contentList[index].classList.add("active");
+  },
+};
